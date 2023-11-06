b27f51141becc6a86f8dcf9d33d0fcf7f770f6b7
Cyber <55909183+scriptkidsensei@users.noreply.github.com>
2023-11-06 19:57:47 +0300

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -3,19 +3,7 @@
 clear
  
 echo "Setup started..."
-yes | sudo apt-get install python
-yes | sudo apt-get install nodejs
-yes | sudo apt-get install npm
- 
-clear
- 
-sudo pacman -S python --no-confirm
-sudo pacman -S nodejs --no-confirm
-sudo pacman -S npm --no-confirm
-
-clear
-
-sudo npm install cfonts
+pip3 install python-cfonts
  
 sudo pip3 install -r requirements.txt
  
