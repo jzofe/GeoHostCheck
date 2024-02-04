d430dc1c53a1822752464a1189281ac408f773ec
Cyber <55909183+jzofe@users.noreply.github.com>
2024-02-04 20:43:35 +0300

    Update geohc.py

--- geohc.py
+++ geohc.py
@@ -714,7 +714,8 @@ def check_ip_protocol_from_args(target_ip, protocol):
         current_proxy_index = 0
 
  while current_proxy_index < len(proxy_list):
-        proxy = proxy_list[current_proxy_index]    if ':' in proxy:
+        proxy = proxy_list[current_proxy_index]    
+        if ':' in proxy:
         proxy_address, proxy_port = proxy.split(':')
         user_agent = get_user_agent()
         result = ping_with_proxy(target_ip, proxy_list, proxy_address, proxy_port, protocol, user_agent)
